293242128756ed8e977ba6fa0af62f3abefb2f4b
ashu5398 <agrawalashu945@gmail.com>
2019-11-20 10:58:16 +0530

    RichText Published

--- AndroidCommonLibrary/src/main/java/com/example/commonlibrary/ACLBindingAdapters.kt
+++ AndroidCommonLibrary/src/main/java/com/example/commonlibrary/ACLBindingAdapters.kt
@@ -1,4 +1,6 @@
 package com.example.commonlibrary
 
 class ACLBindingAdapters {
+
+
 }
--- app/src/main/java/com/example/librarytest/MainActivity.kt
+++ app/src/main/java/com/example/librarytest/MainActivity.kt
@@ -1,30 +1,34 @@
 package com.example.librarytest
 
-import android.os.Build.VERSION_CODES.O
 import android.os.Bundle
-import android.view.View
-import androidx.annotation.RequiresApi
+import android.text.method.LinkMovementMethod
 import androidx.appcompat.app.AppCompatActivity
-import com.example.commonlibrary.ACL
-import com.example.commonlibrary.ACLNotification
+import com.example.commonlibrary.richtext.*
+import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
-    private var acl:ACL = ACL()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        acl.startEngine(applicationContext)
+
+        val text = RichText().make(
+            TextSpan("I have read and accept the", TextStyle(bold = true, underline = true, fontSize = 20),false),
+            TextSpan(" terms and condition.", TextStyle(textColor = "#149fcc",link = "https://www.google.com"),false)
+        )
+        tv_test.text = text
+        tv_test.movementMethod = LinkMovementMethod()
+//        acl.startEngine(applicationContext)
 
         //findViewById<TextView>(R.id.tv_sum).text = aclNotification.sum(2,3).toString()
 
 
     }
 
-    @RequiresApi(O)
+/*    @RequiresApi(O)
     fun doTask(view: View) {
         ACLNotification().createIndeterminateProgressNotification(acl,R.drawable.ic_notifications_black_24dp,12,"Hi","Test Notification",2000)
 
-    }
+    }*/
 }
